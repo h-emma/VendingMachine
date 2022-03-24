a4026b2dafd68b1fd1a6c0bbc2b3385c06332d41
h-emma <emmah92@hotmail.com>
2022-03-24 14:51:36 +0100

    Add program

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var vendingMachine = new Machine();
+var user = new User("Emma", 120);
+
+Console.WriteLine("---Welcome to the Vending Machine---");
+Console.WriteLine("What would you like to buy?");
+foreach (var Slot in vendingMachine.Slots)
+{
+    Console.WriteLine(Slot.SlotNumber + ":" + Slot.Product.Name + " " + Slot.Product.Price + ":-");
+}
+
+while (true)
+{
+    Console.WriteLine("Please press the number for the product you want");
+    var answer = int.Parse(Console.ReadLine()!);
+
+    if (answer > vendingMachine.Slots.Count)
+    {
+        Console.WriteLine("Number not available");
+        continue;
+    }
+    var slot = vendingMachine.Slots.First(p => p.SlotNumber == answer);
+
+    Console.WriteLine($"You chose {slot.Product.Name}. Please insert {slot.Product.Price}:-");
+
+    do
+    {
+        answer = int.Parse(Console.ReadLine()!);
+
+        if (answer < slot.Product.Price)
+        {
+            Console.WriteLine("Insert more money");
+        }
+    } while (answer < slot.Product.Price);
+
+    Console.WriteLine("Thank you and have a nice day");
+    Console.WriteLine($"* you recived one {slot.Product.Name}");
+    break;
+}
+
 
